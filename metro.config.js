4f0ffecef85194d7b0056c486bed1af133a05f66
arhamhi <arhamhfz77@gmail.com>
2026-06-10 17:59:17 +0500

    fix(metro): block backend/ml/cache folders from Metro crawl on Windows

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,22 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+const escapePath = (value) => value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+const ignoredProjectDirs = [
+  'backend',
+  'ml',
+  'dist',
+  '.expo',
+  '.tmp',
+  '.pytest_cache',
+  '.ruff_cache',
+  '.mypy_cache',
+  '.uv-cache',
+].map((dir) => new RegExp(`${escapePath(path.join(__dirname, dir))}[/\\\\].*`));
+
+config.resolver.blockList = ignoredProjectDirs;
 
 module.exports = withNativeWind(config, { input: './src/styles/global.css' });
